4491b0c1c9bab523f3a4b67bf384da33b2652d61
Dominick Lee <dominicklee@purdue.edu>
2016-09-08 19:53:06 -0400

    Code added
    Initial code added

---
+++ BoilerlinkEmailExtractor/BoilerlinkEmailExtractor/Form1.Designer.vb
@@ -0,0 +1,216 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ListBox1 = New System.Windows.Forms.ListBox
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.lblNumEmails = New System.Windows.Forms.Label
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox
+        Me.WebBrowser2 = New System.Windows.Forms.WebBrowser
+        Me.processEmails = New System.Windows.Forms.Timer(Me.components)
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar
+        Me.chkClp = New System.Windows.Forms.Timer(Me.components)
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(555, 39)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(280, 409)
+        Me.ListBox1.TabIndex = 6
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(136, 13)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(336, 21)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(478, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(45, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Go"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(12, 39)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(534, 402)
+        Me.WebBrowser1.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 16)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(118, 15)
+        Me.Label2.TabIndex = 22
+        Me.Label2.Text = "Boilerlink Club URL:"
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(707, 480)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(128, 23)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Save List"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.Control
+        Me.Button3.Enabled = False
+        Me.Button3.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(246, 447)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(300, 25)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Extract Emails from page"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'lblNumEmails
+        '
+        Me.lblNumEmails.AutoSize = True
+        Me.lblNumEmails.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNumEmails.Location = New System.Drawing.Point(552, 452)
+        Me.lblNumEmails.Name = "lblNumEmails"
+        Me.lblNumEmails.Size = New System.Drawing.Size(107, 15)
+        Me.lblNumEmails.TabIndex = 20
+        Me.lblNumEmails.Text = "Member Emails: 0"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.DetectUrls = False
+        Me.RichTextBox1.Location = New System.Drawing.Point(868, 94)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.Size = New System.Drawing.Size(85, 55)
+        Me.RichTextBox1.TabIndex = 11
+        Me.RichTextBox1.TabStop = False
+        Me.RichTextBox1.Text = ""
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.Location = New System.Drawing.Point(555, 16)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(203, 19)
+        Me.CheckBox1.TabIndex = 5
+        Me.CheckBox1.Text = "Add ""mailto:"" emails in clipboard"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'WebBrowser2
+        '
+        Me.WebBrowser2.IsWebBrowserContextMenuEnabled = False
+        Me.WebBrowser2.Location = New System.Drawing.Point(868, 169)
+        Me.WebBrowser2.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser2.Name = "WebBrowser2"
+        Me.WebBrowser2.ScrollBarsEnabled = False
+        Me.WebBrowser2.Size = New System.Drawing.Size(158, 112)
+        Me.WebBrowser2.TabIndex = 13
+        Me.WebBrowser2.TabStop = False
+        Me.WebBrowser2.WebBrowserShortcutsEnabled = False
+        '
+        'processEmails
+        '
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 486)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(534, 23)
+        Me.ProgressBar1.TabIndex = 23
+        '
+        'chkClp
+        '
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.Filter = "Text Files|*.txt|CSV Files|*.csv|All Files|*.*"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(844, 521)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.WebBrowser2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.lblNumEmails)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Boilerlink Email Extractor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents lblNumEmails As System.Windows.Forms.Label
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents WebBrowser2 As System.Windows.Forms.WebBrowser
+    Friend WithEvents processEmails As System.Windows.Forms.Timer
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents chkClp As System.Windows.Forms.Timer
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+
+End Class
---
+++ BoilerlinkEmailExtractor/BoilerlinkEmailExtractor/Form1.vb
@@ -0,0 +1,149 @@
+﻿Public Class Form1
+    Dim input As String
+    Dim phrase As String
+    Dim Occurrences As Integer
+    Dim charIndex As Integer
+    Dim prevId As String
+    Dim emailIndex As Integer = 0
+    Dim lastClipboard As String = ""
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If TextBox1.Text.StartsWith("https://boilerlink.purdue.edu/organization/") Then
+            If TextBox1.Text.EndsWith("/") Then
+                WebBrowser1.Navigate(TextBox1.Text & "roster/manage")
+            Else
+                WebBrowser1.Navigate(TextBox1.Text & "/roster/manage")
+            End If
+            MsgBox("Great! Please log in to your Boilerlink account. Your login will not be saved.")
+        Else
+            MsgBox("The link entered is not a valid Purdue Boilerlink club page.")
+        End If
+    End Sub
+
+    Private Sub TextBox1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles TextBox1.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            If TextBox1.Text.StartsWith("https://boilerlink.purdue.edu/organization/") Then
+                If TextBox1.Text.EndsWith("/") Then
+                    WebBrowser1.Navigate(TextBox1.Text & "roster/manage")
+                Else
+                    WebBrowser1.Navigate(TextBox1.Text & "/roster/manage")
+                End If
+                MsgBox("Great! Please log in to your Boilerlink account. Your login will not be saved.")
+            Else
+                MsgBox("The link entered is not a valid Purdue Boilerlink club page.")
+            End If
+        End If
+    End Sub
+
+    Private Sub WebBrowser1_DocumentCompleted(ByVal sender As Object, ByVal e As System.Windows.Forms.WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+        ProgressBar1.Value = 0
+        RichTextBox1.Clear()
+        RichTextBox1.Text = WebBrowser1.DocumentText
+        If RichTextBox1.Text.Contains("<TD class=""checkbox""><SPAN><INPUT") Then
+            Button3.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Button3.Enabled = False
+        input = RichTextBox1.Text
+        phrase = "<TD class=""checkbox""><SPAN><INPUT"
+        Occurrences = (input.Length - input.Replace(phrase, String.Empty).Length) / phrase.Length
+        charIndex = 0
+        prevId = ""
+
+        MsgBox("We found " & Occurrences & " member emails on this page.")
+
+        For i As Integer = 0 To (Occurrences - 1) Step 1
+            charIndex = input.IndexOf(phrase, charIndex)
+            Dim rawContent As String = input.Substring(charIndex, 200)
+            'check for duplicate IDs
+            If rawContent <> prevId Then
+                'parse for value to get ID
+                Dim startPos As Integer = rawContent.IndexOf("value=")
+                startPos = startPos + 7 'move cursor past "value="
+                'We have a 7 digit ID to trim
+                Dim id As String = rawContent.Substring(startPos, 7)
+                'Add items to the list
+                ListBox1.Items.Add(id)
+                lblNumEmails.Text = "Member Emails: " & ListBox1.Items.Count
+                prevId = rawContent
+            End If
+            charIndex = charIndex + 1
+        Next
+        ProgressBar1.Maximum = Occurrences
+
+        processEmails.Start()
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            chkClp.Start()
+        Else
+            chkClp.Stop()
+        End If
+    End Sub
+
+    Private Sub processEmails_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles processEmails.Tick
+        Try
+            If emailIndex > (ListBox1.Items.Count - 1) Then
+                ProgressBar1.Value = Occurrences
+                processEmails.Stop()
+                MsgBox("Emails acquired successfully! Remember to acquire emails from all pages.")
+            Else
+                Dim currentID As String = ListBox1.Items.Item(emailIndex)
+                WebBrowser2.Navigate("https://boilerlink.purdue.edu/users/membercard/" & currentID)
+                processEmails.Stop()
+                ProgressBar1.Value = emailIndex
+            End If
+
+        Catch
+        End Try
+    End Sub
+
+    Private Sub WebBrowser2_DocumentCompleted(ByVal sender As Object, ByVal e As System.Windows.Forms.WebBrowserDocumentCompletedEventArgs) Handles WebBrowser2.DocumentCompleted
+        Dim memberEmail As String = WebBrowser2.Document.Body.InnerHtml
+        Dim emailStart As Integer = memberEmail.IndexOf("mailto:") + 7
+        Dim emailEnd As Integer = memberEmail.IndexOf("""", emailStart + 1)
+        memberEmail = memberEmail.Substring(emailStart, (emailEnd - emailStart))
+        ListBox1.Items.Item(emailIndex) = memberEmail
+
+        emailIndex = emailIndex + 1
+        processEmails.Start()
+    End Sub
+
+    Private Sub chkClp_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chkClp.Tick
+        If My.Computer.Clipboard.ContainsText Then
+            Dim currentClp As String = My.Computer.Clipboard.GetText
+            If currentClp <> lastClipboard Then
+                If currentClp.StartsWith("mailto:") Then
+                    ListBox1.Items.Add(currentClp.Substring(7))
+                    My.Computer.Audio.PlaySystemSound(System.Media.SystemSounds.Asterisk)
+                    lastClipboard = currentClp
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        SaveFileDialog1.ShowDialog()
+        If SaveFileDialog1.FileName <> "" Then
+            Try
+                Dim content As String = ""
+                For Each item In ListBox1.Items
+                    content = content & ", " & item
+                Next
+                content = content.Substring(2)
+                My.Computer.FileSystem.WriteAllText(SaveFileDialog1.FileName, content, False, System.Text.Encoding.Default)
+                MsgBox("Filed saved!")
+            Catch ex As Exception
+                MsgBox("Failed to save file. Try another directory.")
+            End Try
+        End If
+    End Sub
+End Class
